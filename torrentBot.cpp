b35a3e2acb41e8bdf1970ad617d115a9c6154bcc
bwagne32 <bwagne32@kent.edu>
2024-01-15 02:41:00 +0000

    initial

---
+++ torrentBot.cpp
@@ -0,0 +1,8 @@
+// I could've done this in the python script but I wanted an excuse to write some C code
+
+#include <iostream>
+
+int main(){
+    system("qbittorrent-nox & ");
+    system("python3 torrentBot.py");
+}
